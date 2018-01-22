e2e875017b747a6c7be922c7bbf074625189fa68
Alison Wood <alisonwood29@gmail.com>
2018-01-22 21:42:24 +0000

    attempting extension with no luck

--- app/src/main/java/example/codeclan/com/wordcounterapp/WordCounter.java
+++ app/src/main/java/example/codeclan/com/wordcounterapp/WordCounter.java
@@ -1,23 +1,40 @@
 package example.codeclan.com.wordcounterapp;
 
+import java.util.HashMap;
+
 /**
  * Created by alisonwood29 on 22/01/2018.
  */
 
 public class WordCounter {
 
-    private String word;
+    private String text;
+    private HashMap<String, Integer> wordCounts;
 
     public WordCounter(String word){
-        this.word = word;
+        this.text = word;
+        this.wordCounts = new HashMap<>();
     }
 
 
     public String getWord() {
-        return this.word;
+        return this.text;
     }
 
     public String wordSize() {
-        return "Number of letters: " + this.word.length();
+        return "Number of letters: " + this.text.length();
     }
+
+    public HashMap<String, Integer> getWordMap(){
+        return this.wordCounts;
+    }
+
+    public void splitText(String text){
+        String[] words = text.split("\\s");
+    }
+
+//    public void incrementWordCount(String text){
+//        String word = text.split();
+//        Integer wordCount = wordCounts.get(word);
+//    }
 }
